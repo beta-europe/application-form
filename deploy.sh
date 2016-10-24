3ed1c7d108fca3a7af808b4ec80bf6bf04d82191
Robert Riemann <robert@riemann.cc>
2016-10-24 23:27:27 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export NODE_ENV=production
 grunt build
 rsync -va dist/ europe@meu-strasbourg.org:/home/europe/projects/apply-meus/
 # this is just in order to minimize queries to node and use apache instead
